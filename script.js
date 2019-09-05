746e7dff07f2275ea5ee018f61836116fa30011f
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-09-05 12:11:23 +0200

    WIP

--- script.js
+++ script.js
@@ -35,11 +35,11 @@ function returnsPromise(data) {
 function getGithubData() {
   axios.get('https://api.github.com/users/Ladrillo')
     .then(response => { // response is what the promise resolved to (what it looks is up to axios)
-    document.body.innerText = response.data.name;
-  })
-  .catch(error => { // error is what the promise rejected to (what it looks like depends on axios)
-    document.body.innerText = error.message;
-  });
+      document.body.innerText = response.data.name;
+    })
+    .catch(error => { // error is what the promise rejected to (what it looks like depends on axios)
+      document.body.innerText = error.message;
+    });
 }
 
 // not gonna work
